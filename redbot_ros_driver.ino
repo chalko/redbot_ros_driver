9cf6b496130a81d92d638e0c12b19b458bcd85c9
Nick Chalko <nick@chalko.com>
2014-03-02 16:53:50 -0800

    Initial

---
+++ redbot_ros_driver.ino
@@ -0,0 +1,111 @@
+#include <ros.h>
+#include <std_msgs/Bool.h>
+#include <ArduinoHardware.h>
+
+#include <RedBot.h>
+#include <RedBotSoftwareSerial.h>
+
+
+
+// Flag for bumpers to send the signal that something's wrong and the motors should
+//  be disabled for now. Set in the bump functions, volatile so the change is sure
+//  to propagate to the loop() function.
+volatile boolean bumped = true;
+
+
+ros::NodeHandle nh;
+
+
+
+// Create an alias for the onboard pushbutton.
+#define BUTTON_PIN 12
+
+// Create an alias for the onboard LED.
+#define LED_PIN 13
+
+
+
+///////////////
+// Motor
+
+// Instantiate the motors.
+RedBotMotor motors;
+
+
+void motorCB( const std_msgs::Bool& motorOn){
+  if (motorOn.data && !bumped)
+  {
+    motors.drive(100);
+
+  }
+  else motors.brake();
+}
+
+ros::Subscriber<std_msgs::Bool> sub("motor", &motorCB );
+
+
+
+// Instantiate a couple of whisker switches. Call bump() when one of them
+//  hits something. There's no stopping you having a different function for
+//  each whisker; I just chose not to.
+RedBotBumper lBumper(10, &bump);
+RedBotBumper rBumper(11, &bump);
+
+
+
+std_msgs::Bool pushed_msg;
+ros::Publisher bumper("bumper", &pushed_msg);
+
+
+
+
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("Setup");
+  nh.initNode();
+  nh.advertise(bumper);
+  nh.subscribe(sub);
+
+  // Set up our two built-in IO devices- the button and the LED.
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+
+  bumped=false;
+
+}
+
+void loop() { 
+  // Wait for the button to be pressed, then turn off the "bumped" flag
+  //  so the motors can run. Also, clear the encoders, so we can track
+  //  our motion.
+  if (bumped == true && digitalRead(10)== HIGH && digitalRead(11) == HIGH)
+  {
+    bumped = false;
+    pushed_msg.data = false;
+    bumper.publish(&pushed_msg);
+
+  } 
+
+  nh.spinOnce();
+}
+
+// This is the function that gets called when we bump something. It
+//  stops the motors, signals that a bump occurred (so loop() doesn't
+//  just start the motors back up), and issues a nasty little tone to
+//  tell the user what's up.
+void bump()
+{
+  Serial.println("Bump");
+  motors.brake();
+  pushed_msg.data = true;
+  bumper.publish(&pushed_msg);
+  bumped = true;
+}
+
+
+
+
+
+
+
